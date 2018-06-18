7d24f19894d0e473ef33b03179f41a68b025660c
Matvey Alekseev <alekseevmatvej@gmail.com>
2018-06-18 19:19:02 +0300

    db

---
+++ db.py
@@ -0,0 +1,13 @@
+import sqlite3
+ 
+conn = sqlite3.connect("mydatabase.db") # или :memory: чтобы сохранить в RAM
+cursor = conn.cursor()
+ 
+# Создание таблицы
+cursor.execute("DROP TABLE chats;")
+cursor.execute("CREATE TABLE chats(chat_id text, is_admin text, UNIQUE(chat_id, is_admin));")
+#cursor.execute("UPDATE chats SET is_admin = '0' where chat_id = '109099327';")
+#cursor.execute("UPDATE chats SET is_admin = '1' where chat_id = '-311521038';")
+
+conn.commit()
+conn.close()
