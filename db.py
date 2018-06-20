ad6fc9479a5fc2e22d7552c4fa948a1f9812a5c8
Matvey Alekseev <alekseevmatvej@gmail.com>
2018-06-20 19:41:23 +0300

    123

--- db.py
+++ db.py
@@ -3,11 +3,16 @@ import sqlite3
 conn = sqlite3.connect("mydatabase.db") # или :memory: чтобы сохранить в RAM
 cursor = conn.cursor()
  
-# Создание таблицы
-#cursor.execute("DROP TABLE chats;")
-#cursor.execute("CREATE TABLE chats(chat_id text, status INTEGER DEFAULT 0, UNIQUE(chat_id) );")
+# Создание таблицы "Чаты"
+cursor.execute("DROP TABLE chats;")
+cursor.execute("CREATE TABLE chats(chat_id text, name text dafault 'Your name', status INTEGER DEFAULT 0, UNIQUE(chat_id) );")
 #моя личка chat_id = '109099327'
-cursor.execute("UPDATE chats SET status = 2 where chat_id = '-241874218';")
+#cursor.execute("UPDATE chats SET status = 2 where chat_id = '-241874218';")
+
+# Создание таблицы "Заказы"
+
+#cursor.execute("DROP TABLE registed_chats;")
+#cursor.execute("CREATE TABLE registed_chats(chat_id text, status INTEGER DEFAULT 0, name text,  UNIQUE(chat_id) );")
 
 conn.commit()
 conn.close()
