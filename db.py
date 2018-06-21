7f62ec96b1922e5eb4ba6400620a2a2f976fab82
Matvey Alekseev <alekseevmatvej@gmail.com>
2018-06-21 12:07:36 +0300

    123

--- db.py
+++ db.py
@@ -4,10 +4,10 @@ conn = sqlite3.connect("mydatabase.db") # или :memory: чтобы сохра�
 cursor = conn.cursor()
  
 # Создание таблицы "Чаты"
-cursor.execute("DROP TABLE chats;")
-cursor.execute("CREATE TABLE chats(chat_id REAL, name text dafault 'Your name', status INTEGER DEFAULT 0, UNIQUE(chat_id) );")
+#cursor.execute("DROP TABLE chats;")
+#cursor.execute("CREATE TABLE chats(chat_id REAL, name text dafault 'Your name', status INTEGER DEFAULT 0, UNIQUE(chat_id) );")
 #моя личка chat_id = '109099327'
-#cursor.execute("UPDATE chats SET status = 2 where chat_id = '-241874218';")
+cursor.execute("UPDATE chats SET status = 2 where chat_id = '-241874218';")
 
 # Создание таблицы "Заказы"
 
