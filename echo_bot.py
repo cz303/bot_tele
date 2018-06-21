6115a2486c9c90328692cd91331982344ab91be4
Matvey Alekseev <alekseevmatvej@gmail.com>
2018-06-21 14:52:13 +0300

    123

--- echo_bot.py
+++ echo_bot.py
@@ -13,6 +13,8 @@ from telegramcalendar import create_calendar
 
 logging.basicConfig(filename="logs/tele_bot.log", level=logging.INFO)
 
+current_shown_dates={}
+
 setmessage = []
 viewstatic = []
 inlk = []
@@ -266,5 +268,65 @@ def dislike(call):
     bot.answer_callback_query(call.id, text="Спасибо за отзыв")
     bot.edit_message_text(call.message.text, call.from_user.id,
                           call.message.message_id, parse_mode='MARKDOWN', disable_web_page_preview=True)
+
+@bot.callback_query_handler(func=lambda call: call.data[0:13] == 'calendar-day-')
+def get_day(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        day=call.data[13:]
+        date = datetime.datetime(int(saved_date[0]),int(saved_date[1]),int(day))
+        bot.edit_message_text("Вы выбрали: *" + str(date.strftime("%d.%m.%Y")) + "*", call.from_user.id, call.message.message_id, parse_mode='MARKDOWN')
+        bot.answer_callback_query(call.id, text="Дата выбрана")
+
+    else:
+        #Do something to inform of the error
+        pass
+
+@bot.callback_query_handler(func=lambda call: call.data == 'next-month')
+def next_month(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        year,month = saved_date
+        month+=1
+        if month>12:
+            month=1
+            year+=1
+        date = (year,month)
+        current_shown_dates[chat_id] = date
+        markup= create_calendar(year,month)
+        bot.edit_message_text("Пожалуйста, выберете дату", call.from_user.id, call.message.message_id, reply_markup=markup)
+        bot.answer_callback_query(call.id, text="")
+    else:
+        #Do something to inform of the error
+        pass
+
+@bot.callback_query_handler(func=lambda call: call.data == 'previous-month')
+def previous_month(call):
+    chat_id = call.message.chat.id
+    saved_date = current_shown_dates.get(chat_id)
+    if(saved_date is not None):
+        year,month = saved_date
+        month-=1
+        if month<1:
+            month=12
+            year-=1
+        date = (year,month)
+        current_shown_dates[chat_id] = date
+        markup= create_calendar(year,month)
+        bot.edit_message_text("Пожалуйста, выберете дату", call.from_user.id, call.message.message_id, reply_markup=markup)
+        bot.answer_callback_query(call.id, text="")
+    else:
+        #Do something to inform of the error
+        pass
+
+@bot.callback_query_handler(func=lambda call: call.data == 'ignore')
+def ignore(call):
+    bot.answer_callback_query(call.id, text="")
+
+@bot.callback_query_handler(func=lambda call: call.data == 'less_day')
+def less_day(call):
+    bot.answer_callback_query(call.id, text="Дата должна быть позже сегодня")
 
 bot.polling()
