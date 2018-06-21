604edec85a3ff2162bbc5116b149ba82c6ab445f
Matvey Alekseev <alekseevmatvej@gmail.com>
2018-06-21 14:22:13 +0300

    123

--- telegramcalendar.py
+++ telegramcalendar.py
@@ -31,7 +31,7 @@ def create_calendar(year,month):
     #Last row - Buttons
     row=[]
     now = datetime.now()
-    if datetime.strptime(str(now.month)+"."+str(now.year), "%m.%Y") > datetime.strptime(str(month)+"."+str(year), "%m.%Y"):
+    if datetime.strptime(str(now.month)+"."+str(now.year), "%m.%Y") < datetime.strptime(str(month)+"."+str(year), "%m.%Y"):
         row.append(types.InlineKeyboardButton("<",callback_data="previous-month"))
     else:
         row.append(types.InlineKeyboardButton(" ", callback_data="ignore"))
