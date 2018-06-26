5e8aebb32d40595923d20d546d7b790a8209f5ef
Matvey Alekseev <alekseevmatvej@gmail.com>
2018-06-26 12:14:09 +0300

    123

--- dbelem.py
+++ dbelem.py
@@ -4,13 +4,13 @@ conn = sqlite3.connect("elemdatabase.db") # или :memory: чтобы сохр�
 cursor = conn.cursor()
  
 # Создание таблицы "Чаты"
-cursor.execute("DROP TABLE chats;")
+#cursor.execute("DROP TABLE chats;")
 cursor.execute("CREATE TABLE chats(chat_id real, name text DEFAULT 'Your name', status INTEGER DEFAULT 0, UNIQUE(chat_id) );")
 #моя личка chat_id = '109099327'
 #cursor.execute("UPDATE chats SET status = 2 where chat_id = -241874218;")
 
 # Создание таблицы "Статистики"
-cursor.execute("DROP TABLE stats;")
+#cursor.execute("DROP TABLE stats;")
 cursor.execute("CREATE TABLE stats(stat text, number INTEGER DEFAULT 0, name text,  UNIQUE(stat) );")
 cursor.execute("INSERT INTO stats(stat,name) VALUES ('likes','Лайков');")
 cursor.execute("INSERT INTO stats(stat,name) VALUES ('dislikes','Дизлайков');")
@@ -20,7 +20,7 @@ cursor.execute("INSERT INTO stats(stat,name) VALUES ('orders_send','Всего �
 
 
 # Создание таблицы "Заказы"
-cursor.execute("DROP TABLE orders;")
+#cursor.execute("DROP TABLE orders;")
 cursor.execute("CREATE TABLE orders(chat_id real, status INTEGER DEFAULT 0, header text DEFAULT 'None',date text DEFAULT 'None',time text DEFAULT 'None',place text DEFAULT 'None',comment text DEFAULT 'None',customer text DEFAULT 'None',phone_number text DEFAULT 'None');")
 
 conn.commit()
