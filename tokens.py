35791bd58b2effb51d33c8817119c24180792850
Matvey Alekseev <alekseevmatvej@gmail.com>
2018-06-26 12:14:17 +0300

    123

--- tokens.py
+++ tokens.py
@@ -1,1 +1,2 @@
-telegrambot = "575202317:AAGlHvAyBjPlBlOS0oxLOB6toarGV51HxVQ"
+telegrambot_test = "575202317:AAGlHvAyBjPlBlOS0oxLOB6toarGV51HxVQ"
+telegrambot_elem = "420685937:AAGQPM_E6O6HNBjv90u7TSMIxUJzAxO-P0o"
